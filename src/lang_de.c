c5e6d49ca76a66841a40f6d903932b9c618aadaf
Berggold digital <mail@berggold.digital>
2025-02-23 01:18:49 +0100

    Update lang_de.c

--- src/lang_de.c
+++ src/lang_de.c
@@ -1,1 +1,13 @@
+#include "lang.h"
 
+/* Based on BIP-39 (unchanged) */
+
+POLYSEED_PRIVATE const polyseed_lang polyseed_lang_de = {
+    .name = "Deutsch",
+    .name_en = "German",
+    .separator = " ",
+    .is_sorted = true,
+    .has_prefix = true,
+    .has_accents = false,
+    .compose = false,
+    .words = {
